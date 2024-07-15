5b9085f84e06a6de1609ebcead5e8f5dc74ebf7f
Your Name <basmalaelshabrawy37@gmai.com>
2024-07-15 14:08:14 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int globalint = 0;    
+        static void Main(string[] args)
+        {
+
+        }
+        static List<int> ii(List<int> list)
+        {
+            return list.Where(x => x % 2 == 0).ToList();
+        }
+        public static void IncrementCounter()
+        {
+            globalint++;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp = new List<Employee>
+            {
+                new Employee {FName = "Basmala", LName = "Mohamed", Salary=15.000m},
+                new Employee {FName = "sara", LName = "Ahmed", Salary =20.000m}
+            };
+            var re = emp.EmployeeFilter();
+            foreach (var r in re)
+            {
+                Console.WriteLine(r);
+            }
+            Console.ReadLine();
+
+        }
+        public static IEnumerable<string> EmployeeFilter(this IEnumerable<Employee> emp)
+        {
+            return emp.
+                Where(e => e.Salary > 15.000m).Select(e => $"{e.FName} {e.LName}");
+        }
+    }
+    class Employee
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
